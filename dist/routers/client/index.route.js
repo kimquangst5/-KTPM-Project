c435deef8f02d7dc7b892950988b55a3b21eee97
Trần Kim Quang <kimquangst5@gmail.com>
2025-06-16 15:11:04 +0700

    up code 15:10  16/06/2025

--- dist/routers/client/index.route.js
+++ dist/routers/client/index.route.js
@@ -8,7 +8,9 @@ const products_route_1 = __importDefault(require("./products.route"));
 const accounts_route_1 = __importDefault(require("./accounts.route"));
 const cart_route_1 = __importDefault(require("./cart.route"));
 const orders_route_1 = __importDefault(require("./orders.route"));
+const check_login_middleware_1 = require("../../middlewares/client/check_login.middleware");
 const index = (app) => {
+    app.use(check_login_middleware_1.check_login_validate);
     app.use("", home_route_1.default);
     app.use("/san-pham", products_route_1.default);
     app.use("/tai-khoan", accounts_route_1.default);
--- routers/client/index.route.ts
+++ routers/client/index.route.ts
@@ -4,8 +4,10 @@ import products from "./products.route";
 import accounts from "./accounts.route";
 import carts from "./cart.route";
 import orders from "./orders.route";
+import { check_login_validate } from '../../middlewares/client/check_login.middleware';
 
 const index = (app: Application) => {
+    app.use(check_login_validate)
     app.use("", home);
     app.use("/san-pham", products);
     app.use("/tai-khoan", accounts);
